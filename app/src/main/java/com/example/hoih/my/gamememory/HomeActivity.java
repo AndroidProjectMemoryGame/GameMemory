00c1ad1eb39afe83566ec61bd7ee09a5c064e4b9
an.ta <an.ta@pn.com.vn>
2018-12-29 16:26:27 +0700

    navigate select mode

--- app/src/main/java/com/example/hoih/my/gamememory/HomeActivity.java
+++ app/src/main/java/com/example/hoih/my/gamememory/HomeActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class HomeActivity extends Activity {
 
-    Button btnNormal, btnNoLimitTime;
+    Button btnNormal, btnNoLimitTime, btnHard;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,22 +17,31 @@ public class HomeActivity extends Activity {
         setContentView(R.layout.activity_home);
         btnNormal = (Button)findViewById(R.id.btn_normal);
         btnNoLimitTime = (Button)findViewById(R.id.btn_no_limit_time);
-
-        btnNormal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(HomeActivity.this, NormalActivity.class);
-                startActivity(i);
-            }
-        });
+        btnHard = (Button)findViewById(R.id.btn_hard);
 
         btnNoLimitTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(HomeActivity.this, NormalActivity.class);
+                Intent i = new Intent(HomeActivity.this, ModeActivity.class);
+                i.putExtra("mode", "1");
                 startActivity(i);
             }
         });
-
+        btnNormal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, ModeActivity.class);
+                i.putExtra("mode", "2");
+                startActivity(i);
+            }
+        });
+        btnHard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, ModeActivity.class);
+                i.putExtra("mode", "3");
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hoih/my/gamememory/ModeActivity.java
+++ app/src/main/java/com/example/hoih/my/gamememory/ModeActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -12,9 +11,9 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class NormalActivity extends Activity {
+public class ModeActivity extends Activity {
 
-    TextView txtIconBack;
+    TextView txtIconBack, txtMode;
     ListView levelListView;
     ArrayList<Level> levelData = new ArrayList<>();
 
@@ -24,6 +23,7 @@ public class NormalActivity extends Activity {
         setContentView(R.layout.activity_normal);
         levelListView = (ListView)findViewById(R.id.listView);
         txtIconBack = (TextView)findViewById(R.id.txt_icon_back);
+        txtMode = (TextView)findViewById(R.id.txt_mode);
 
         txtIconBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +31,16 @@ public class NormalActivity extends Activity {
                 finish();
             }
         });
+
+        if (getIntent() != null) {
+            if (getIntent().getStringExtra("mode").equals("1")) {
+                txtMode.setText("No Limit Time");
+            } else if (getIntent().getStringExtra("mode").equals("2")) {
+                txtMode.setText("Normal");
+            } else {
+                txtMode.setText("Hard");
+            }
+        }
 
         levelData = getData();
 
@@ -40,7 +50,7 @@ public class NormalActivity extends Activity {
         levelListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Intent i = new Intent(NormalActivity.this, LevelDetailActivity.class);
+                Intent i = new Intent(ModeActivity.this, LevelDetailActivity.class);
                 i.putExtra("level",levelData.get(position).getLevel());
                 startActivity(i);
             }
